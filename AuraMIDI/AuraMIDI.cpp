743fd1be1892dfdf20f39b918ebcbe1f5838e808
abhi-s1n <www.abhinav1@gmail.com>
2024-01-26 15:55:32 +0530

    update pattern tiles and color logic

--- AuraMIDI/AuraMIDI.cpp
+++ AuraMIDI/AuraMIDI.cpp
@@ -23,6 +23,24 @@ static int maxContour(std::vector<std::vector<cv::Point>>& contours)
 	return maxAreaIndex;
 }
 
+static void setGreen(std::vector<cv::Scalar>& tileColor, int index, bool isMute = false)
+{
+	cv::Scalar grey(122, 122, 122);
+	cv::Scalar green(0, 256, 0);
+	cv::Scalar red(0, 0, 256);
+
+	for (int i = 0; i < tileColor.size(); i++)
+	{
+		tileColor[i] = grey;
+	}
+	if (isMute == true)
+	{
+		tileColor[index] = red;
+		return;
+	}
+	tileColor[index] = green;
+}
+
 int main() {
 	cv::Mat image;
 	cv::Mat mask;
@@ -57,6 +75,14 @@ int main() {
 	createAndSetTrackbar("Lower Saturation", "Set HSV", obj[4], 255);
 	createAndSetTrackbar("Lower Value", "Set HSV", obj[5], 255);
 
+	cv::Scalar grey(122, 122, 122);
+	cv::Scalar green(0, 256, 0);
+	cv::Scalar red(0, 0, 256);
+	cv::Scalar white(256, 256, 256);
+
+	std::vector<cv::Scalar> patColor(5, grey);
+	std::vector<cv::Scalar> trkColor(4, grey);
+
 	while (true)
 	{
 		cap >> image;
@@ -64,24 +90,19 @@ int main() {
 		cv::flip(image, image, 1);
 		cv::flip(mask, mask, 1);
 
-		cv::Scalar grey(122, 122, 122);
-		cv::Scalar green(0, 256, 0);
-		cv::Scalar red(0, 0, 256);
-		cv::Scalar white(256, 256, 256);
-
 		// Adding the colour buttons to the live frame for colour access
 		// Patterns
-		cv::rectangle(image, cv::Point(80, 1), cv::Point(160, 80), grey, -1);
-		cv::rectangle(image, cv::Point(175, 1), cv::Point(255, 80), grey, -1);
-		cv::rectangle(image, cv::Point(270, 1), cv::Point(350, 80), grey, -1);
-		cv::rectangle(image, cv::Point(365, 1), cv::Point(445, 80), grey, -1);
-		cv::rectangle(image, cv::Point(460, 1), cv::Point(540, 80), grey, -1);
+		cv::rectangle(image, cv::Point(80, 1), cv::Point(160, 80), patColor[0], -1);
+		cv::rectangle(image, cv::Point(175, 1), cv::Point(255, 80), patColor[1], -1);
+		cv::rectangle(image, cv::Point(270, 1), cv::Point(350, 80), patColor[2], -1);
+		cv::rectangle(image, cv::Point(365, 1), cv::Point(445, 80), patColor[3], -1);
+		cv::rectangle(image, cv::Point(460, 1), cv::Point(540, 80), patColor[4], -1);
 
 		// Tracks
-		cv::rectangle(image, cv::Point(1, 80), cv::Point(80, 160), grey, -1);
-		cv::rectangle(image, cv::Point(1, 175), cv::Point(80, 255), grey, -1);
-		cv::rectangle(image, cv::Point(1, 270), cv::Point(80, 350), grey, -1);
-		cv::rectangle(image, cv::Point(1, 365), cv::Point(80, 445), grey, -1);
+		cv::rectangle(image, cv::Point(1, 80), cv::Point(80, 160), trkColor[0], -1);
+		cv::rectangle(image, cv::Point(1, 175), cv::Point(80, 255), trkColor[1], -1);
+		cv::rectangle(image, cv::Point(1, 270), cv::Point(80, 350), trkColor[2], -1);
+		cv::rectangle(image, cv::Point(1, 365), cv::Point(80, 445), trkColor[3], -1);
 
 		// Image Processing
 		int u_hue = cv::getTrackbarPos("Upper Hue", "Set HSV");
@@ -126,31 +147,54 @@ int main() {
 				if ((80 <= center.y) && (center.y <= 160))
 				{
 					track = 80;
-					cv::rectangle(image, cv::Point(1, 80), cv::Point(80, 160), green, -1);
+					setGreen(trkColor, 0);
 				}
 				else if ((175 <= center.y) && (center.y <= 255))
 				{
 					track = 70;
-					cv::rectangle(image, cv::Point(1, 175), cv::Point(80, 255), green, -1);
+					setGreen(trkColor, 1);
 				}
 				else if ((270 <= center.y) && (center.y <= 350))
 				{
 					track = 60;
-					cv::rectangle(image, cv::Point(1, 270), cv::Point(80, 350), green, -1);
+					setGreen(trkColor, 2);
 				}
 				else if ((365 <= center.y) && (center.y <= 445))
 				{
 					track = 50;
-					cv::rectangle(image, cv::Point(1, 365), cv::Point(80, 445), green, -1);
+					setGreen(trkColor, 3);
+				}
+			}
+			if (center.y <= 80)
+			{
+				if ((80 <= center.x) && (center.x <= 160))
+				{
+					setGreen(patColor, 0);
+				}
+				else if ((175 <= center.x) && (center.x <= 255))
+				{
+					setGreen(patColor, 1);
+				}
+				else if ((270 <= center.x) && (center.x <= 350))
+				{
+					setGreen(patColor, 2);
+				}
+				else if ((365 <= center.x) && (center.x <= 445))
+				{
+					setGreen(patColor, 3);
+				}
+				else if ((460 <= center.x) && (center.x <= 540))
+				{
+					setGreen(patColor, 4, true);
 				}
 			}
 		}
 
 		// Tile Text
-		cv::putText(image, "Pat. 1", cv::Point(96, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
-		cv::putText(image, "Pat. 2", cv::Point(190, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
-		cv::putText(image, "Pat. 3", cv::Point(290, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
-		cv::putText(image, "Pat. 4", cv::Point(380, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "PAT 1", cv::Point(96, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "PAT 2", cv::Point(190, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "PAT 3", cv::Point(290, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "PAT 4", cv::Point(380, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
 		cv::putText(image, "MUTE", cv::Point(480, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
 
 		cv::putText(image, "TRACK 1", cv::Point(8, 115), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
@@ -167,6 +211,9 @@ int main() {
 			break;
 		}
 	}
+
+	cap.release();
+	cv::destroyAllWindows();
 
 	return 0;
 }
