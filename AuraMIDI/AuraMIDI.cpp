4020ac0f232db97ab2df694753b1d3633c45b5d6
abhi-s1n <www.abhinav1@gmail.com>
2024-01-26 15:28:14 +0530

    add tile updation and tile text

--- AuraMIDI/AuraMIDI.cpp
+++ AuraMIDI/AuraMIDI.cpp
@@ -27,6 +27,7 @@ int main() {
 	cv::Mat image;
 	cv::Mat mask;
 	cv::VideoCapture cap(0);
+	int track = 80;
 
 	if (!cap.isOpened())
 	{
@@ -66,23 +67,21 @@ int main() {
 		cv::Scalar grey(122, 122, 122);
 		cv::Scalar green(0, 256, 0);
 		cv::Scalar red(0, 0, 256);
-
-		std::vector<cv::Scalar> patColors(5, grey);
-		std::vector<cv::Scalar> trkColors(4, grey);
+		cv::Scalar white(256, 256, 256);
 
 		// Adding the colour buttons to the live frame for colour access
 		// Patterns
-		cv::rectangle(image, cv::Point(80, 1), cv::Point(160, 80), patColors[0], -1);
-		cv::rectangle(image, cv::Point(175, 1), cv::Point(255, 80), patColors[1], -1);
-		cv::rectangle(image, cv::Point(270, 1), cv::Point(350, 80), patColors[2], -1);
-		cv::rectangle(image, cv::Point(365, 1), cv::Point(445, 80), patColors[3], -1);
-		cv::rectangle(image, cv::Point(460, 1), cv::Point(540, 80), patColors[4], -1);
+		cv::rectangle(image, cv::Point(80, 1), cv::Point(160, 80), grey, -1);
+		cv::rectangle(image, cv::Point(175, 1), cv::Point(255, 80), grey, -1);
+		cv::rectangle(image, cv::Point(270, 1), cv::Point(350, 80), grey, -1);
+		cv::rectangle(image, cv::Point(365, 1), cv::Point(445, 80), grey, -1);
+		cv::rectangle(image, cv::Point(460, 1), cv::Point(540, 80), grey, -1);
 
 		// Tracks
-		cv::rectangle(image, cv::Point(1, 80), cv::Point(80, 160), trkColors[0], -1);
-		cv::rectangle(image, cv::Point(1, 175), cv::Point(80, 255), trkColors[1], -1);
-		cv::rectangle(image, cv::Point(1, 270), cv::Point(80, 350), trkColors[2], -1);
-		cv::rectangle(image, cv::Point(1, 365), cv::Point(80, 445), trkColors[3], -1);
+		cv::rectangle(image, cv::Point(1, 80), cv::Point(80, 160), grey, -1);
+		cv::rectangle(image, cv::Point(1, 175), cv::Point(80, 255), grey, -1);
+		cv::rectangle(image, cv::Point(1, 270), cv::Point(80, 350), grey, -1);
+		cv::rectangle(image, cv::Point(1, 365), cv::Point(80, 445), grey, -1);
 
 		// Image Processing
 		int u_hue = cv::getTrackbarPos("Upper Hue", "Set HSV");
@@ -121,7 +120,43 @@ int main() {
 			float radius;
 			cv::minEnclosingCircle(cnt, center, radius);
 			cv::circle(image, center, int(radius), cv::Scalar(0, 255, 255), 2);
+
+			if (center.x <= 80)
+			{
+				if ((80 <= center.y) && (center.y <= 160))
+				{
+					track = 80;
+					cv::rectangle(image, cv::Point(1, 80), cv::Point(80, 160), green, -1);
+				}
+				else if ((175 <= center.y) && (center.y <= 255))
+				{
+					track = 70;
+					cv::rectangle(image, cv::Point(1, 175), cv::Point(80, 255), green, -1);
+				}
+				else if ((270 <= center.y) && (center.y <= 350))
+				{
+					track = 60;
+					cv::rectangle(image, cv::Point(1, 270), cv::Point(80, 350), green, -1);
+				}
+				else if ((365 <= center.y) && (center.y <= 445))
+				{
+					track = 50;
+					cv::rectangle(image, cv::Point(1, 365), cv::Point(80, 445), green, -1);
+				}
+			}
 		}
+
+		// Tile Text
+		cv::putText(image, "Pat. 1", cv::Point(96, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "Pat. 2", cv::Point(190, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "Pat. 3", cv::Point(290, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "Pat. 4", cv::Point(380, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "MUTE", cv::Point(480, 33), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+
+		cv::putText(image, "TRACK 1", cv::Point(8, 115), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "TRACK 2", cv::Point(8, 210), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "TRACK 3", cv::Point(8, 305), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
+		cv::putText(image, "TRACK 4", cv::Point(8, 400), cv::FONT_HERSHEY_SIMPLEX, 0.5, white, 1, cv::LINE_AA);
 
 		// Display
 		imshow("Display Mask", mask);
